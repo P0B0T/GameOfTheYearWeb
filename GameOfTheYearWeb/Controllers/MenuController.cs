d182399707d17186d624bc3cc8059f50865f6876
P0B0T <P0B0T@P0B0T>
2024-07-28 23:00:45 +0300

    Добавьте файлы проекта.

---
+++ GameOfTheYearWeb/Controllers/MenuController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameOfTheYearWeb.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Menu() => View();
+    }
+}
